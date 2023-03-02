e0225d7d55fa2206859964de894d8f9753e82b91
eunji9677 <eunji71234@gmail.com>
2023-03-02 10:04:01 +0900

    front Test

---
+++ front/src/components/PageFooter.vue
@@ -0,0 +1,22 @@
+<template>
+    <div>
+        <hr/> 
+        <footer> 
+        
+        </footer>
+        <div><img src="../assets/logo03.png"></div>
+   </div>  
+  </template>
+
+<script>
+ export default {} 
+ </script>
+ <style scoped> 
+footer{
+    background:#4bbe09;
+    padding: .5px;
+}
+
+div{
+    text-align: right;
+}</style>
---
+++ front/src/components/PageHeader.vue
@@ -0,0 +1,23 @@
+<template>
+    <header>
+     
+        <h5><b>인사 괸리</b></h5>
+        
+    </header>
+    <hr/>
+</template>
+  
+<script>
+  export default {
+  
+  }
+</script>
+  
+<style scoped>
+  header {
+    text-align: center;
+    padding:25px;
+    border-bottom: 1px solid #4bbe09;
+    background:#f7f7f7;
+}
+</style>
