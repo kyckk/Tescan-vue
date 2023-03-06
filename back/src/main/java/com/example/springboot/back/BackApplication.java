2c2d091b424bdd8b0d5e2f2f19917f51a10e6f08
eunji9677 <eunji71234@gmail.com>
2023-03-06 10:12:44 +0900

    error

--- back/src/main/java/com/example/springboot/back/BackApplication.java
+++ back/src/main/java/com/example/springboot/back/BackApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = {"com.example.springboot.back.customer.domain"})
 @EnableAutoConfiguration(exclude={DataSourceAutoConfiguration.class})
 public class BackApplication {
 
